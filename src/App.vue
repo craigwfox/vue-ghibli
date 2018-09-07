18df19adffba2ff57d1eb82f4391993405387d50
Craig Fox <cwfox@arkbluecross.com>
2018-09-07 11:06:43 -0500

    setting up repo

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="film-wrapper">
+    <h1>Studio Ghibli Films</h1>
+    <ul class="film-nav">
+      <li><a href="#0">Person 1</a></li>
+      <li><a href="#0">Person 2</a></li>
+      <li><a href="#0">Person 3</a></li>
+    </ul>
+    <article class="film" v-for="film in films">
+      <header class="film-title">
+        <h2> 
+          <a href="#0">{{film.title}}</a>
+        </h2>
+        <ul>
+          <li><strong>Director:</strong> {{film.director}}</li>
+          <li><strong>Producer:</strong> {{film.producer}}</li>
+          <li><strong>Release:</strong> {{film.release_date}}</li>
+          <li><strong>Rotten Tomatoes:</strong> {{film.rt_score}}</li>
+        </ul>
+      </header>
+      <p class="film-description">{{film.description}}</p>
+    </article>
+  </section>
+</template>
+
+<script>
+import axios from 'axios'
+// import FilmComponent from './components/FilmComponent.vue'
+export default {
+  // components: {
+  //   FilmComponent
+  // },
+  data() {
+    return {
+      test: 'do this',
+      films: [],
+      errors: []
+    }
+  },
+  mounted () {
+    axios
+      .get('https://ghibliapi.herokuapp.com/films')
+      .then(response => {
+      this.films = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+  }
+}
+</script>
---
+++ src/components/FilmComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <article class="film">
+    <header class="film-title">
+      <h2> 
+        <a href="#0">{{film.title}}</a>
+      </h2>
+      <ul>
+        <li><strong>Director:</strong> {{film.director}}</li>
+        <li><strong>Producer:</strong> {{film.producer}}</li>
+        <li><strong>Release:</strong> {{film.release_date}}</li>
+        <li><strong>Rotten Tomatoes:</strong> {{film.rt_score}}</li>
+      </ul>
+    </header>
+    <p class="film-description">{{film.description}}</p>
+  </article>
+</template>
+<script>
+export default {
+  props: {
+    film: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
