2dc7b819f93fa4d81e4a449adc44499d22c7c3e1
Craig Fox <cwfox@arkbluecross.com>
2018-09-19 14:44:13 -0500

    scoped styles and added active states to nav

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <section class="film-wrapper">
     <h1>Studio Ghibli Films</h1>
     <ul class="film-nav">
-      <li><a href="#all" data-id="all" @click="navDirector">All</a></li>
+      <li class="active"><a href="#all" data-id="all" @click="navDirector">All</a></li>
       <directors-component @click.native="navDirector" v-for="director in directors" :key="director[director]" :director-data="director"></directors-component>
     </ul>
 
@@ -11,84 +11,90 @@
 </template>
 
 <script>
-import axios from 'axios'
-import FilmComponent from './components/FilmComponent.vue'
-import DirectorsComponent from './components/DirectorsComponent.vue'
-export default {
-  components: {
-    FilmComponent,
-    DirectorsComponent
-  },
-  data() {
-    return {
-      test: 'do this',
-      films: [],
-      directors: [],
-      errors: []
-    }
-  },
-  mounted () {
-    axios
-      .get('https://ghibliapi.herokuapp.com/films')
-      .then(response => {
-        let filmsArry = [];
-        response.data.forEach(element => {
-          element.show = true;
-          filmsArry.push(element);
-        })
-        this.films = filmsArry;
-        this.getDirectors();
-    })
-    .catch(e => {
-      this.errors.push(e)
-    })
-  },
-  methods: {
-    getDirectors: function() {
-      let directArry = [];
-      this.films.forEach(element => {
-        let elDir = {
-          name: element.director,
-          id: element.id,
-          url: element.director.replace(/ /g,'-').toLowerCase()
-        };
-
-        let dirfound = false;
-        for(let i = 0; i < directArry.length; i++) {
-          if (directArry[i].name == elDir.name) {
-            dirfound = true;
-            break;
-          }
-        }
-
-        if (!dirfound) {
-          directArry.push(elDir);
-        }
-      });
-
-      directArry.sort();
-      this.directors = directArry;
+  import axios from 'axios'
+  import FilmComponent from './components/FilmComponent.vue'
+  import DirectorsComponent from './components/DirectorsComponent.vue'
+  export default {
+    components: {
+      FilmComponent,
+      DirectorsComponent
     },
-    navDirector: function(e) {
-      let curid = e.target.getAttribute('data-id');
-      console.log(curid);
-
-      if(curid === 'all') {
-        console.log('show:', curid);
-        this.films.forEach(element => {
-          element.show = true;
-        })
-      } else {
-        console.log('show:', curid);
-        this.films.forEach(element => {
-          if (element.director === curid) {
-            element.show = true;
-          } else {
-            element.show = false;
-          }
-        })
+    data() {
+      return {
+        test: 'do this',
+        films: [],
+        directors: [],
+        errors: []
       }
     },
+    mounted () {
+      axios
+        .get('https://ghibliapi.herokuapp.com/films')
+        .then(response => {
+          let filmsArry = [];
+          response.data.forEach(element => {
+            element.show = true;
+            filmsArry.push(element);
+          })
+          this.films = filmsArry;
+          this.getDirectors();
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+    },
+    methods: {
+      getDirectors: function() {
+        let directArry = [];
+        this.films.forEach(element => {
+          let elDir = {
+            name: element.director,
+            id: element.id,
+            url: element.director.replace(/ /g,'-').toLowerCase()
+          };
+
+          let dirfound = false;
+          for(let i = 0; i < directArry.length; i++) {
+            if (directArry[i].name == elDir.name) {
+              dirfound = true;
+              break;
+            }
+          }
+
+          if (!dirfound) {
+            directArry.push(elDir);
+          }
+        });
+
+        directArry.sort();
+        this.directors = directArry;
+      },
+      navDirector: function(e) {
+        let curid = e.target.getAttribute('data-id');
+        let filmnav = document.querySelectorAll('.film-nav li');
+
+        filmnav.forEach(navel => {
+          navel.classList.remove('active');
+        })
+
+        e.target.closest('li').classList.add('active');
+
+        if(curid === 'all') {
+          console.log('show:', curid);
+          this.films.forEach(element => {
+            element.show = true;
+          })
+        } else {
+          console.log('show:', curid);
+          this.films.forEach(element => {
+            if (element.director === curid) {
+              element.show = true;
+            } else {
+              element.show = false;
+            }
+          })
+        }
+      },
+    }
   }
-}
 </script>
--- src/components/FilmComponent.vue
+++ src/components/FilmComponent.vue
@@ -25,3 +25,55 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+/* film item */
+.film {
+  display: flex;
+  align-items: stretch;
+
+  padding: 3rem;
+}
+
+/* Film Title */
+.film-title {
+  flex: 1 0 30%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  
+  padding-right: 2rem;
+  
+  border-right: 1px solid var(--orange);
+}
+
+.film-title h2 {
+  margin: 0;
+  
+  font-size: 2.3rem;
+}
+
+.film-title h2,
+.film-title h2 a {
+  color: var(--blue);  
+}
+
+.film-title ul {
+  margin: 1rem 0 0;
+  padding: 0;
+  
+  list-style-type: none;
+}
+
+/* Film Description */
+.film-description {
+  flex: 1 1 auto;
+  display: flex;
+  align-items: center;
+  
+  margin: 0;
+  padding-left: 2rem;
+  
+  font-size: 1.8rem;
+}
+</style>
